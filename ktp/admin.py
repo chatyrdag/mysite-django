4701faf7351bec34e32168e579f129182db4b00f
Alexei Petrash <alexei@alex-math.ru>
2020-10-27 19:01:17 +0300

    Изменена админка. Добавлены фильтры

--- ktp/admin.py
+++ ktp/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import AcademicYear, Teacher, SchoolForm, Discipline, KTP
 
 
+class KTPAdmin(admin.ModelAdmin):
+	list_display = ('number_of_lesson', 'plan_date', 'theme',)
+	list_filter = ('school_form', 'discipline_name',)
+
+
 admin.site.register(AcademicYear)
 admin.site.register(Teacher)
 admin.site.register(SchoolForm)
 admin.site.register(Discipline)
-admin.site.register(KTP)
+admin.site.register(KTP, KTPAdmin)
