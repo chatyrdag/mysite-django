c08e3fd880aa0367237d760b54ece718f9e7abe5
Alexei Petrash <alexei@alex-math.ru>
2020-12-13 21:00:40 +0300

    Добавлены в админку новые модели из модуля GIA

--- gia/admin.py
+++ gia/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import GiaTask
+from .models import GiaTask, Exam, TaskType
 
 
 admin.site.register(GiaTask)
+admin.site.register(Exam)
+admin.site.register(TaskType)
