aef70189a7ac2d4fface713aa08209fe349bab15
Alexei <alexei@alex-math.ru>
2021-01-11 10:57:31 +0300

    Добавил login_reqiured для функции train

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def main(request):
     return render(request, 'base.html')
 
 
-def train(request):
-    return render(request, 'train.html')
+@login_required
+def train(request, train_type):
+    return render(request, 'train.html', {'train_type': train_type})
